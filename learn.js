17ecb6b9806d94d6e39bdc170f89a34770e4390b
charlesnyanje <charleswilliam26@gmail.com>
2023-01-25 12:18:12 +0300

    initial commit

---
+++ learn.js
@@ -0,0 +1,28 @@
+let names = [
+  "charles",
+  "david",
+  "james",
+  "john",
+  "joseph",
+  "michael",
+  "richard",
+  "robert",
+  "thomas",
+  "william",
+];
+
+
+
+
+
+
+const payLunch = () => {
+  names.forEach((name) => {
+
+
+   
+    console.log(` ${name} has paid for lunch`);
+  });
+};
+
+payLunch();
